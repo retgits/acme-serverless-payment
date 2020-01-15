d98ad75e91ce7508968beb70ca794c82445a82a9
retgits <lstigter@gmail.com>
2020-01-15 15:22:47 -0800

    feat: add mage

--- deploy/mage/mage.go
+++ deploy/mage/mage.go
@@ -3,114 +3,20 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"regexp"
-	"strings"
+	"path"
 
-	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
-	"gopkg.in/yaml.v2"
 )
 
-// Go is the namespace to group all Go commands
-type Go mg.Namespace
-
-// AWS is the namespace to group all AWS commands
-type AWS mg.Namespace
-
-// Pulumi is the namespace to group all Pulumi commands
-type Pulumi mg.Namespace
-
-// ConfigRoot is the root element in the Pulumi configuration file
-type ConfigRoot struct {
-	Config Config `yaml:"config"`
-}
-
-// Config contains all configuration items as key/value pairs, where the key is made up
-// of <namespace>:<key>
-type Config struct {
-	AwsProfile string `yaml:"aws:profile"`
-	AwsRegion  string `yaml:"aws:region"`
-	AwsBucket  string `yaml:"aws:bucket"`
-	TagsStage  string `yaml:"tags:stage"`
-	TagsAuthor string `yaml:"tags:author"`
-	TagsTeam   string `yaml:"tags:team"`
-	MageTests  string `yaml:"mage:tests"`
-}
-
-var config = getConfig()
-
-func getConfig() Config {
-	// Get the stack name
-	stack := os.Getenv("stack")
-
-	// Read the Pulumi YAML file for the stack
-	fileContent, err := ioutil.ReadFile(fmt.Sprintf("Pulumi.%s.yaml", stack))
-	if err != nil {
-		panic(err)
-	}
-	source := string(fileContent)
-
-	// Find all configuration variables that have been set as
-	// environment variables
-	re := regexp.MustCompile(`\$\$.*\$\$`)
-	vars := re.FindAllString(source, -1)
-
-	// Replace the configuration variables with the actual values
-	// of the environment variable
-	for _, v := range vars {
-		source = strings.ReplaceAll(source, v, getEnvVar(strings.ReplaceAll(v, "$$", "")))
-	}
-
-	// Unmarshal the YAML content to a proper struct and return
-	// the config part of it
-	d := &ConfigRoot{}
-	yaml.Unmarshal([]byte(source), d)
-	return d.Config
-}
-
-func getEnvVar(envvar string) string {
-	b, found := os.LookupEnv(envvar)
-	if !found {
-		return "unknown"
-	}
-	return b
-}
-
-func getBasepath() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		panic(fmt.Errorf("cannot get base folder: %s", err))
-	}
-	return dir
-}
-
-func getGitVersion() string {
-	v := run("git", "describe", "--tags", "--always", "--dirty=-dev")
-	if len(v) == 0 {
-		v = "dev"
-	}
-	return v
-}
-
-func run(name string, arg ...string) string {
-	buf := &bytes.Buffer{}
-	c := exec.Command(name, arg...)
-	c.Stdout = buf
-	c.Stderr = os.Stderr
-	c.Run()
-	return strings.TrimSuffix(buf.String(), "\n")
-}
+var apps = []string{"lambda-eventbridge", "lambda-sqs"}
 
 // Deps resolves and downloads dependencies to the current development module and then builds and installs them.
 // Deps will rely on the Go environment variable GOPROXY (go env GOPROXY) to determine from where to obtain the
 // sources for the build.
-func (Go) Deps() error {
-	goProxy := run("go", "env", "GOPROXY")
+func Deps() error {
+	goProxy, _ := sh.Output("go", "env", "GOPROXY")
 	fmt.Printf("Getting Go modules from %s", goProxy)
 	return sh.Run("go", "get", "./...")
 }
@@ -118,6 +24,73 @@ func (Go) Deps() error {
 // 'Go test' automates testing the packages named by the import paths. go:test compiles and tests each of the
 // packages listed on the command line. If a package test passes, go test prints only the final 'ok' summary
 // line.
-func (Go) Test() error {
+func Test() error {
 	return sh.RunV("go", "test", "-cover", "./...")
+}
+
+// Vuln uses Snyk to test for any known vulnerabilities in go.mod. The command relies on access to the Snyk.io
+// vulnerability database, so it cannot be used without Internet access.
+func Vuln() error {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	return sh.RunV("snyk", "test", fmt.Sprintf("--file=%s", path.Join(workingDir, "..", "..", "go.mod")))
+}
+
+// Build compiles the individual commands in the cmd folder, along with their dependencies. All built executables
+// are stored in the 'bin' folder. Specifically for deployment to AWS Lambda, GOOS is set to linux and GOARCH is
+// set to amd64.
+func Build() error {
+	if !appExists(config.App) {
+		return fmt.Errorf("app %s does not exist as valid target", config.App)
+	}
+
+	env := make(map[string]string)
+	env["GOOS"] = "linux"
+	env["GOARCH"] = "amd64"
+
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	return sh.RunWith(env, "go", "build", "-o", path.Join(workingDir, "..", "..", "cmd", config.App, "bin", config.App), path.Join(workingDir, "..", "..", "cmd", config.App))
+}
+
+// Clean removes object files from package source directories.
+func Clean() error {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	return sh.Rm(path.Join(workingDir, "..", "..", "cmd", config.App, "bin"))
+}
+
+// Deploy packages, deploys, and returns all outputs of your stack. Packages the local artifacts (local paths) that your
+// AWS CloudFormation template references and uploads  local  artifacts to an S3 bucket. The command returns a copy of your
+// template, replacing references to local artifacts with the S3 location where the command uploaded the artifacts. Deploys
+// the specified AWS CloudFormation template by creating and then executing a change set. The command terminates after AWS
+// CloudFormation executes  the change set. Returns the description for the specified stack.
+func Deploy() error {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	if err := sh.RunV("aws", "cloudformation", "package", "--template-file", path.Join(workingDir, "..", "..", "cmd", config.App, "template.yaml"), "--output-template-file", path.Join(workingDir, "..", "..", "cmd", config.App, "packages.yaml"), "--s3-bucket", config.AWS.Bucket); err != nil {
+		return err
+	}
+
+	if err := sh.RunV("aws", "cloudformation", "deploy", "--template-file", path.Join(workingDir, "..", "..", "cmd", config.App, "packages.yaml"), "--stack-name", fmt.Sprintf("%s-%s", config.Project, config.Stage), "--capabilities", "CAPABILITY_IAM", "--parameter-overrides", fmt.Sprintf("Version=%s", config.Version), fmt.Sprintf("Author=%s", config.Author), fmt.Sprintf("Team=%s", config.Team)); err != nil {
+		return err
+	}
+
+	if err := sh.RunV("aws", "cloudformation", "describe-stacks", "--stack-name", fmt.Sprintf("%s-%s", config.Project, config.Stage), "--query", "'Stacks[].Outputs'"); err != nil {
+		return err
+	}
+
+	return nil
 }
