553ae543b824f292a302ec3c8857299092ad1acb
retgits <lstigter@gmail.com>
2020-02-21 17:28:53 -0800

    chore: add sentry breadcrumb

--- cmd/lambda-payment-sqs/main.go
+++ cmd/lambda-payment-sqs/main.go
@@ -53,23 +53,41 @@ func handler(request events.SQSEvent) error {
 		},
 	}
 
+	crumb := sentry.Breadcrumb{
+		Category:  "CreditCardValidated",
+		Timestamp: time.Now().Unix(),
+		Level:     sentry.LevelInfo,
+		Data: map[string]interface{}{
+			"Amount":  req.Request.Total,
+			"OrderID": req.Request.OrderID,
+			"Success": true,
+			"Message": "transaction successful",
+		},
+	}
+
 	validator := validator.New()
 	err = validator.Creditcard(req.Request.Card)
 	if err != nil {
-		sentry.CaptureException(fmt.Errorf("validation failed for order [%s] : %s", req.Request.OrderID, err.Error()))
-		log.Printf("Validation failed: %s", err.Error())
 		evt.Metadata.Status = "error"
 		evt.Data.Success = false
 		evt.Data.Status = http.StatusBadRequest
 		evt.Data.Message = "creditcard validation has failed, unable to process payment"
 		evt.Data.TransactionID = "-1"
+		crumb.Data["Success"] = evt.Data.Success
+		crumb.Data["Message"] = evt.Data.Message
+		sentry.CaptureException(fmt.Errorf("validation failed for order [%s] : %s", req.Request.OrderID, err.Error()))
+		log.Printf("Validation failed: %s", err.Error())
 	}
+
+	sentry.AddBreadcrumb(&crumb)
 
 	err = em.Send(evt)
 	if err != nil {
 		sentry.CaptureException(fmt.Errorf("error sending CreditCardValidated event: %s", err.Error()))
 		return err
 	}
+
+	sentry.CaptureMessage(fmt.Sprintf("validation successful for order [%s]", req.Request.OrderID))
 
 	return nil
 }
